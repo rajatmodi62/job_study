f0eee2f6a9549af5eafaf9775e31dbb326e6a396
rmodi <rajatmodi62@gmail.com>
2026-04-09 22:17:05 -0400

    a

--- distributed/ddp-tutorial-series/multigpu_torchrun.py
+++ distributed/ddp-tutorial-series/multigpu_torchrun.py
@@ -13,10 +13,14 @@ import os
 
 
 #setting up a process is neveccary 
+# wow this is very simple, so nice. 
+
 def ddp_setup():
     torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
     init_process_group(backend="nccl")
 
+
+# no need to pass gpu_id
 class Trainer:
     def __init__(
         self,
@@ -26,6 +30,7 @@ class Trainer:
         save_every: int,
         snapshot_path: str,
     ) -> None:
+
         self.gpu_id = int(os.environ["LOCAL_RANK"])
         self.model = model.to(self.gpu_id)
         self.train_data = train_data
@@ -33,6 +38,7 @@ class Trainer:
         self.save_every = save_every
         self.epochs_run = 0
         self.snapshot_path = snapshot_path
+
         if os.path.exists(snapshot_path):
             print("Loading snapshot")
             self._load_snapshot(snapshot_path)
@@ -40,13 +46,19 @@ class Trainer:
         self.model = DDP(self.model, device_ids=[self.gpu_id])
 
     def _load_snapshot(self, snapshot_path):
+
         loc = f"cuda:{self.gpu_id}"
+
         snapshot = torch.load(snapshot_path, map_location=loc)
+
         self.model.load_state_dict(snapshot["MODEL_STATE"])
+
         self.epochs_run = snapshot["EPOCHS_RUN"]
+
         print(f"Resuming training from snapshot at Epoch {self.epochs_run}")
 
     def _run_batch(self, source, targets):
+
         self.optimizer.zero_grad()
         output = self.model(source)
         loss = F.cross_entropy(output, targets)
@@ -54,23 +66,32 @@ class Trainer:
         self.optimizer.step()
 
     def _run_epoch(self, epoch):
+
         b_sz = len(next(iter(self.train_data))[0])
         print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+
         self.train_data.sampler.set_epoch(epoch)
+        
         for source, targets in self.train_data:
+
             source = source.to(self.gpu_id)
             targets = targets.to(self.gpu_id)
             self._run_batch(source, targets)
 
+
     def _save_snapshot(self, epoch):
+
         snapshot = {
             "MODEL_STATE": self.model.module.state_dict(),
             "EPOCHS_RUN": epoch,
         }
+
         torch.save(snapshot, self.snapshot_path)
+
         print(f"Epoch {epoch} | Training snapshot saved at {self.snapshot_path}")
 
     def train(self, max_epochs: int):
+
         for epoch in range(self.epochs_run, max_epochs):
             self._run_epoch(epoch)
             if self.gpu_id == 0 and epoch % self.save_every == 0:
@@ -78,13 +99,18 @@ class Trainer:
 
 
 def load_train_objs():
+
     train_set = MyTrainDataset(2048)  # load your dataset
+
     model = torch.nn.Linear(20, 1)  # load your model
+
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
     return train_set, model, optimizer
 
 
 def prepare_dataloader(dataset: Dataset, batch_size: int):
+
     return DataLoader(
         dataset,
         batch_size=batch_size,
@@ -95,6 +121,7 @@ def prepare_dataloader(dataset: Dataset, batch_size: int):
 
 
 def main(save_every: int, total_epochs: int, batch_size: int, snapshot_path: str = "snapshot.pt"):
+
     ddp_setup()
     dataset, model, optimizer = load_train_objs()
     train_data = prepare_dataloader(dataset, batch_size)
@@ -104,6 +131,7 @@ def main(save_every: int, total_epochs: int, batch_size: int, snapshot_path: str
 
 
 if __name__ == "__main__":
+
     import argparse
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
--- distributed/ddp-tutorial-series/rm_run_single_gpu.sh
+++ distributed/ddp-tutorial-series/rm_run_single_gpu.sh
@@ -2,4 +2,8 @@
 # python single_gpu.py --total_epochs 50 --save_every 5 --batch_size 128
 
 #multiple gpu 
-python multigpu.py  5000000  5 
+# python multigpu.py  5000000  5 --batch_size 128
+
+
+# torch run with interrupt training
+torchrun --standalone --nproc_per_node=2  multigpu_torchrun.py  5000000  5 --batch_size 128
