5cead8049fa9e141ead1962df3160201ae90e72c
rmodi <rajatmodi62@gmail.com>
2026-04-09 20:14:27 -0400

    a

--- distributed/ddp-tutorial-series/rm_run_single_gpu.sh
+++ distributed/ddp-tutorial-series/rm_run_single_gpu.sh
@@ -2,4 +2,4 @@
 # python single_gpu.py --total_epochs 50 --save_every 5 --batch_size 128
 
 #multiple gpu 
-python multigpu.py --total_epochs 5000000 --save_every 5 --batch_size 128
+python multigpu.py  5000000  5 --batch_size 128
